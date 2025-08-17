6f822e5f27edfa4abdc9dacdf7a3c5990ec11d16
Tobi Joshua <tobilobasam3@gmail.com>
2025-08-17 16:25:56 -0700

    constant updated

--- frontend/frontend/src/constants.js
+++ frontend/frontend/src/constants.js
@@ -1,2 +1,5 @@
 // src/constants.js
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://vyzioads.pythonanywhere.com"
+    : "http://127.0.0.1:8000";
