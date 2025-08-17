df02fdb632b7fd036c7b484e9cfa93d83ce3ab2f
Tobi Joshua <tobilobasam3@gmail.com>
2025-08-17 15:12:48 -0700

    Updated urls

--- vyzio_backend/urls.py
+++ vyzio_backend/urls.py
@@ -3,22 +3,19 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('ads.urls')), 
-    path("", TemplateView.as_view(template_name="index.html")),
-
-    # Fix favicon path
-    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
+    path('api/', include('ads.urls')),
+    re_path(r"^(?!api/|static/|media/|favicon.ico).*", TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
     # Serve media files during development
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # Serve media files in production (if you want to, but usually handled by web server)
+    # Serve media files in production (usually handled by web server)
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
